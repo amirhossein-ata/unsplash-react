e22dc2ded9f6ec1daa2f36d36abdecefd6af0e1c
Amirhossein <amirhossein1398@gmail.com>
2021-04-28 23:21:02 +0430

    Create Poject

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="flex">
+        <div className="h-6 w-6 bg-gray-100"></div>
+        <div className="h-6 w-6 bg-red-200"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
